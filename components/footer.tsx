7e021e7291f65e84d99b1b6f2403de65a7d50d83
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2023-08-22 17:08:12 -0400

    feat: Add copyright

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,7 +1,13 @@
+// TODO Add link to https://github.com/steezplusplus
+
 export function Footer() {
   return (
-    <footer className='border-t bg-white'>
-      <p>Placeholder footer</p>
+    <footer className='bg-white border-t'>
+      <div className="mx-auto py-10">
+        <p className="text-center text-xs text-black">
+          &copy; SteezPlusPlus. All rights reserverd.
+        </p>
+      </div>
     </footer>
   );
 }
