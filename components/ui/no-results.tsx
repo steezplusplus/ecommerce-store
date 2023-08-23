814fda2be988a63519ff67dc6fadeaf9a87cacfc
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2023-08-23 16:07:52 -0400

    feat: create no results component

---
+++ components/ui/no-results.tsx
@@ -0,0 +1,7 @@
+export function NoResults() {
+  return (
+    <div className='flex items-center justify-center h-full w-full'>
+      <p className='text-neutral-500'>No results found.</p>
+    </div>
+  );
+}
