d797e7a12a62e15213870816847590c1007d367b
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2023-08-23 14:16:55 -0400

    feat: Create route to fetch a billboard

---
+++ actions/get-billboard.tsx
@@ -0,0 +1,12 @@
+import { Billboard } from "@/types";
+
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
+const URL = `${API_URL}/billboards`;
+
+export async function getBillboard(id: string): Promise<Billboard> {
+  const res = await fetch(`${URL}/${id}`);
+  if (!res.ok) {
+    throw new Error(`[GET_BILLBOARD]: ${res.statusText}`);
+  }
+  return res.json();
+}
