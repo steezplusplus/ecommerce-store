cba384be50e80763c86931f5effb7d05ff9af6c0
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2023-08-24 16:50:59 -0400

    feat: Rework layout of category grid

--- app/(routes)/category/[categoryId]/page.tsx
+++ app/(routes)/category/[categoryId]/page.tsx
@@ -6,6 +6,7 @@ import { Container } from "@/components/ui/container";
 import { NoResults } from "@/components/ui/no-results";
 import { Billboard } from "../../components/billboard";
 import { Filter } from "./components/filter";
+import { ProductCard } from "@/components/ui/product-card";
 
 export const revalidate = 0;
 
@@ -38,26 +39,32 @@ export default async function CategoryPage(props: CategoryPageProps) {
     <div>
       <Container>
         <Billboard data={category.billboard} />
-      </Container>
-      <div className="px-4 sm:px-6 lg:px-8 pb-24">
-        <div className="lg:frid lg:grid-cols-5 lg:gap-x-8">
-          <div className="hidden lg:block">
-            <Filter
-              valueKey="sizeId"
-              name="Sizes"
-              data={sizes}
-            />
-            <Filter
-              valueKey="colorId"
-              name="Colors"
-              data={colors}
-            />
-          </div>
-          <div className="mt-6 lg:col-span-4 lg:mt-0">
-            {products.length === 0 && <NoResults />}
+        <div className="px-4 sm:px-6 lg:px-8 pb-24">
+          <div className="lg:grid lg:grid-cols-5 lg:gap-x-8">
+            <div className="hidden lg:block">
+              <Filter
+                valueKey="sizeId"
+                name="Sizes"
+                data={sizes}
+              />
+              <Filter
+                valueKey="colorId"
+                name="Colors"
+                data={colors}
+              />
+            </div>
+            <div className="mt-6 lg:col-span-4 lg:mt-0">
+              {products.length === 0 && <NoResults />}
+              <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4'>
+                {products.map((product) => (
+                  <ProductCard key={product.id} data={product} />
+                ))}
+              </div>
+            </div>
           </div>
         </div>
-      </div>
+      </Container>
+
     </div>
   );
 }
