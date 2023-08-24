be5f57ba8dff090a02ca9f2e9220d75dbae29d9b
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2023-08-24 19:53:38 -0400

    feat: Create modal store, component and provider

---
+++ hooks/use-preview-modal.ts
@@ -0,0 +1,17 @@
+import { create } from 'zustand';
+
+import { Product } from '@/types';
+
+interface PreviewModalStore {
+  isOpen: boolean;
+  data?: Product;
+  onOpen: (data: Product) => void;
+  onClose: () => void;
+};
+
+export const usePreviewModal = create<PreviewModalStore>((set) => ({
+  isOpen: false,
+  data: undefined,
+  onOpen: (data: Product) => set({ data, isOpen: true }),
+  onClose: () => set({ isOpen: false }),
+}));
---
+++ providers/modal-provider.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+import { PreviewModal } from "@/components/ui/preview-modal";
+
+export function ModalProvider() {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return null;
+  }
+
+  return (
+    <>
+      <PreviewModal />
+    </>
+  );
+}
