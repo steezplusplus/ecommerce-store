2e13f1ae43e379365011ebd4925f1b935226a3fc
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2023-08-26 18:11:29 -0400

    feat: Navbar counts items in cart

--- app/components/nav-actions.tsx
+++ app/components/nav-actions.tsx
@@ -4,9 +4,11 @@ import { useState, useEffect } from 'react';
 import { ShoppingBag } from 'lucide-react';
 
 import { Button } from '@/components/ui/button';
+import { useCart } from '@/hooks/use-cart';
 
 export function NavActions() {
   const [isMounted, setIsMounted] = useState<boolean>(false);
+  const cart = useCart();
 
   useEffect(() => {
     setIsMounted(true);
@@ -21,7 +23,9 @@ export function NavActions() {
       <li className='ml-auto pr-4'>
         <Button className='flex items-center rounded-full bg-black px-4 py-2'>
           <ShoppingBag size={20} color='white' />
-          <span className='ml-2 text-sm font-medium text-white'>0</span>
+          <span className='ml-2 text-sm font-medium text-white'>
+            {cart.items.length}
+          </span>
         </Button>
       </li>
     </>
