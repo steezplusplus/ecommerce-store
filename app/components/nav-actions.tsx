f968458429eff288356d29707e96c3092987917c
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2023-08-26 18:37:25 -0400

    feat: Create cart page and link to cart page

--- app/components/nav-actions.tsx
+++ app/components/nav-actions.tsx
@@ -1,18 +1,25 @@
 'use client';
 
+import { useRouter } from 'next/navigation';
 import { useState, useEffect } from 'react';
 import { ShoppingBag } from 'lucide-react';
 
 import { Button } from '@/components/ui/button';
 import { useCart } from '@/hooks/use-cart';
 
+// TODO Replace Button with Link
 export function NavActions() {
   const [isMounted, setIsMounted] = useState<boolean>(false);
+  const router = useRouter();
   const cart = useCart();
 
   useEffect(() => {
     setIsMounted(true);
   }, []);
+
+  const onCheckout = () => {
+    router.push('/cart')
+  }
 
   if (!isMounted) {
     return null; // Prevent server from generating html
@@ -21,7 +28,9 @@ export function NavActions() {
   return (
     <>
       <li className='ml-auto pr-4'>
-        <Button className='flex items-center rounded-full bg-black px-4 py-2'>
+        <Button
+          onClick={onCheckout}
+          className='flex items-center rounded-full bg-black px-4 py-2 cursor-pointer'>
           <ShoppingBag size={20} color='white' />
           <span className='ml-2 text-sm font-medium text-white'>
             {cart.items.length}
