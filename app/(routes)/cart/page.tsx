40dbc6b18c74b85caedb9d82a12850f4f8c7d4ed
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2023-08-26 18:48:20 -0400

    feat: Show items in cart

--- app/(routes)/cart/page.tsx
+++ app/(routes)/cart/page.tsx
@@ -1,7 +1,42 @@
+"use client";
+
+import { useEffect, useState } from 'react';
+
+import { Container } from '@/components/ui/container';
+import { useCart } from '@/hooks/use-cart';
+
+export const revalidate = 0;
+
 export default function CartPage() {
+  const [isMounted, setIsMounted] = useState(false);
+  const cart = useCart();
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return null;
+  }
+
   return (
-    <div>
-      Cart
+    <div className="bg-white">
+      <Container>
+        <div className="px-4 py-16 sm:px-6 lg:px-8">
+          <h1 className="text-3xl font-bold text-black">Shopping Cart</h1>
+          <div className="mt-12 lg:grid lg:grid-cols-12 lg:items-start gap-x-12">
+            <div className="lg:col-span-7">
+              {cart.items.length === 0 && <p className="text-neutral-500">No items added to cart.</p>}
+              <ul>
+                {cart.items.map((item) => (
+                  // <CartItem key={item.id} data={item} />
+                  <p key={item.id}>{item.name}</p>
+                ))}
+              </ul>
+            </div>
+          </div>
+        </div>
+      </Container>
     </div>
-  );
-}
+  )
+};
