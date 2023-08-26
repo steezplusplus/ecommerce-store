1e85e3a1b8adfaceb1191e3e22de2b010a500d5f
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2023-08-26 17:01:33 -0400

    style: Update billboard typography

--- app/(routes)/components/billboard.tsx
+++ app/(routes)/components/billboard.tsx
@@ -14,9 +14,9 @@ export function Billboard(props: BillboardProps) {
         style={{ backgroundImage: `url(${data?.imageUrl})` }}
       >
         <div className='flex h-full w-full flex-col items-center justify-center gap-y-8 text-center'>
-          <div className='max-w-xs text-3xl font-bold sm:max-w-xl sm:text-5xl lg:text-6xl'>
+          <p className='text-white max-w-xs text-3xl font-bold sm:max-w-xl sm:text-5xl lg:text-6xl drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]'>
             {data.label}
-          </div>
+          </p>
         </div>
       </div>
     </div>
