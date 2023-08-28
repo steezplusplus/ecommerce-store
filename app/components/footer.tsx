f2ed5f06e7483e66ecfdff43e19d2757d335b11c
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2023-08-28 15:47:54 -0400

    feat: Update footer styles and add link to github

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,13 +1,25 @@
-// TODO Add link to https://github.com/steezplusplus
+import { Container } from '@/components/ui/container';
 
 export function Footer() {
   return (
-    <footer className='border-t bg-white'>
-      <div className='mx-auto py-10'>
-        <p className='text-center text-xs text-black'>
-          &copy; SteezPlusPlus. All rights reserverd.
-        </p>
-      </div>
+    <footer className='border-t'>
+      <Container>
+        <ul className='flex items-center gap-x-2 px-4 py-2 sm:gap-x-4 sm:px-6 lg:gap-x-6 lg:px-8'>
+          <li>
+            &copy; Jesse Breuer-Penello. All rights reserverd.
+          </li>
+          <li className='ml-auto'>
+            <a
+              href='https://github.com/steezplusplus/ecommerce-store'
+              target='_blank'
+              rel='noopener noreferrer'
+              className='text-blue-600 hover:underline flex items-center'
+            >
+              View source code
+            </a>
+          </li>
+        </ul>
+      </Container>
     </footer>
   );
 }
