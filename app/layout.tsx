2b6ec5b07ef7168e364810179f126737b91b7b06
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2023-08-29 14:06:04 -0400

    refactor: Re-organize imports

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,11 @@
-import './globals.css';
 import type { Metadata } from 'next';
 import { Urbanist } from 'next/font/google';
 
-import { Footer } from './components/footer';
-import { Nav } from './components/nav';
 import { ModalProvider } from '@/providers/modal-provider';
 import { ToastProvider } from '@/providers/toast-provider';
+import { Nav } from './components/nav';
+import { Footer } from './components/footer';
+import './globals.css';
 
 const font = Urbanist({ subsets: ['latin'] });
 
@@ -13,11 +13,13 @@ export const metadata: Metadata = {
   title: 'Store',
 };
 
-export default function RootLayout({
-  children,
-}: {
+type RootLayoutProps = {
   children: React.ReactNode;
-}) {
+};
+
+export default function RootLayout(props: RootLayoutProps){
+  const { children } = props;
+
   return (
     <html lang='en'>
       <body className={font.className}>
