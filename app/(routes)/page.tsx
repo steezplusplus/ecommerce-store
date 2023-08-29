46228991bc1e92acc259292e0f73eb28957a173c
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2023-08-29 13:56:13 -0400

    style: Move flex

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -15,11 +15,12 @@ export const metadata: Metadata = {
 export default async function HomePage() {
   const products = await getProducts({ isFeatured: true });
   const billboard = await getBillboard('3622c3a7-e75b-4e84-a3bd-9107e76e4430'); // TODO pull primary billboard id out to env var?
+
   return (
     <Container>
-      <div className='space-y-10 pb-10'>
+      <div className='flex flex-col gap-y-10 pb-10'>
         <Billboard data={billboard} />
-        <div className='flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8'>
+        <div className='px-4 sm:px-6 lg:px-8'>
           <ProductList title='Featured Products' items={products} />
         </div>
       </div>
