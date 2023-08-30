7cbbfe34d3fad6786f7f5e5d129f6655b62f5c3d
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2023-08-29 21:29:43 -0400

    style: less border radius on loading ui

--- app/(routes)/loading.tsx
+++ app/(routes)/loading.tsx
@@ -4,23 +4,21 @@ export default function Loading() {
   return (
     <div>
       {/* Billboard */}
-      <div className='rounded-xl'>
-        <LoadingSkeleton className='aspect-square w-full rounded-xl md:aspect-[2.4/1]' />
-      </div>
+      <LoadingSkeleton className='aspect-square w-full md:aspect-[2.4/1]' />
 
       {/* Heading */}
       <div className='h-[76px]' />
 
       {/* Product List */}
       <div className='grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4'>
-        <LoadingSkeleton className='aspect-square rounded-xl' />
-        <LoadingSkeleton className='aspect-square rounded-xl' />
-        <LoadingSkeleton className='aspect-square rounded-xl' />
-        <LoadingSkeleton className='aspect-square rounded-xl' />
-        <LoadingSkeleton className='aspect-square rounded-xl' />
-        <LoadingSkeleton className='aspect-square rounded-xl' />
-        <LoadingSkeleton className='aspect-square rounded-xl' />
-        <LoadingSkeleton className='aspect-square rounded-xl' />
+        <LoadingSkeleton className='aspect-square' />
+        <LoadingSkeleton className='aspect-square' />
+        <LoadingSkeleton className='aspect-square' />
+        <LoadingSkeleton className='aspect-square' />
+        <LoadingSkeleton className='aspect-square' />
+        <LoadingSkeleton className='aspect-square' />
+        <LoadingSkeleton className='aspect-square' />
+        <LoadingSkeleton className='aspect-square' />
       </div>
     </div>
   );
