c60ad07996035306c35d78c44bdb8c6034000a3a
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2023-08-30 15:53:00 -0400

    fix: Pass missing prop

--- components/preview-modal.tsx
+++ components/preview-modal.tsx
@@ -14,13 +14,10 @@ export function PreviewModal() {
   }
 
   return (
-    <Modal
-      open={previewModal.isOpen}
-      onClose={previewModal.onClose}
-    >
+    <Modal open={previewModal.isOpen} onClose={previewModal.onClose}>
       <div className='grid w-full grid-cols-1 items-start gap-x-6 gap-y-8 sm:grid-cols-12 lg:gap-x-8'>
         <div className='sm:col-span-4 lg:col-span-5'>
-          <Gallery images={product.images} />
+          <Gallery name={product.name} images={product.images} />
         </div>
         <div className='sm:col-span-8 lg:col-span-7'>
           <Info data={product} />
